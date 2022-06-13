b029823eb31f8847cfe29f225343bd1aa7168be5
wrainf <amirfaiz1806@gmail.com>
2022-06-13 10:32:45 +0800

    completed coloring function

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
 //dimension of canvas
 let dimension = 16;
+let color = 'aqua';
+let canvas = document.createElement('div');
+
 
 const body = document.querySelector('body');
+const setNewDimension = document.querySelector('#new-dimension');
+const toggleRainbow = document.querySelector('#rainbow');
+const toggleDefault = document.querySelector('#single-color');
+const resetButton = document.querySelector('#reset');
 
+
+setNewDimension.addEventListener('click',()=>{
+    canvas.removeChild(canvas.firstElementChild);
+    let promptMSG = "Enter New Dimension, Max: 64"
+    dimension = prompt(promptMSG);
+    while(dimension < 1 || dimension > 64 || dimension == null){
+        dimension = prompt(promptMSG);
+    }
+    let grid = createGrid(dimension);
+
+    canvas.appendChild(grid);
+})
 
 function createGrid(dimension){
     let grid = document.createElement('div');
-    grid.style.cssText = 'display:flex; flex-direction: column';
+    grid.style.cssText = 'border: 2px solid black; width: 600px; height: 600px;display:flex; flex-direction: column';
     for (let x = 1; x <= dimension; x++) {
         let row = document.createElement('div');
         row.style.cssText = 'display:flex';
+        let boxDimension = 600/dimension;
 
         for (let y = 1; y <= dimension; y++) {
             let box = document.createElement('div');
-            box.style.cssText = 'border: 2px solid black; width:20px; height:20px';
+            box.style.width = boxDimension+'px';
+            box.style.height = boxDimension+'px';
+            box.setAttribute("onmouseover","colorBox(this,color)");
             row.appendChild(box);
             
         }
@@ -25,5 +47,13 @@ function createGrid(dimension){
 }
 
 let grid = createGrid(dimension);
+canvas.appendChild(grid);
 
-body.appendChild(grid);
+
+body.appendChild(canvas);
+
+
+
+function colorBox(box, color){
+    box.style.backgroundColor = color;
+}
